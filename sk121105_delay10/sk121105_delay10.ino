5de14611eb550249f5edff79b628d9af23881cff
Kenichi Ota <byh10071@gmail.com>
2026-04-15 10:12:20 +0900

    Add files via upload

---
+++ sk121105_delay10/sk121105_delay10.ino
@@ -0,0 +1,14 @@
+#define SENSOR 0
+int val = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()>0) {
+    val = analogRead(SENSOR);
+    Serial.println(val);
+    delay(10);
+  }
+}
